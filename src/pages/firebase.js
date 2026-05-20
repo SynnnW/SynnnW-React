eb373975be03dae3acf506edfdd28b69d8772998
SynnnW <aldo.leo88@sma.belajar.id>
2026-05-20 08:46:30 +0700

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,8 +1,8 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Konfigurasi resmi dari Firebase Console milikmu
+// Konfigurasi resmi dari Firebase Console
 const firebaseConfig = {
   apiKey: "AIzaSyCUUtQZbqd5yjJgBMlwjNEo5JGIRcgKowI",
   authDomain: "synnnw-85363.firebaseapp.com",
@@ -10,12 +10,15 @@ const firebaseConfig = {
   storageBucket: "synnnw-85363.firebasestorage.app",
   messagingSenderId: "625609420361",
   appId: "1:625609420361:web:137d5b46fe6e96bb33d998",
-  measurementId: "G-95CV0YB7FK"
+  measurementId: "G-95CV0YB7FK",
 };
 
 // Inisialisasi Firebase
 const app = initializeApp(firebaseConfig);
 
-// Export agar bisa dibaca oleh Contact.jsx milikmu
-export const auth = getAuth(app);
+// Auth
+export const auth     = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+// Firestore
 export const db = getFirestore(app);
