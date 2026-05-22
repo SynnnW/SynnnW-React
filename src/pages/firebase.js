2045843006d55df7e24b3c3df8c5d8c2db98d973
SynnnW <aldo.leo88@sma.belajar.id>
2026-05-22 14:45:18 +0700

    Update firebase.js

--- src/pages/firebase.js
+++ src/pages/firebase.js
@@ -1,6 +1,7 @@
-// src/pages/firebase.js  ← PAKAI VERSI INI
+// src/pages/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUUtQZbqd5yjJgBMlwjNEo5JGIRcgKowI",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
